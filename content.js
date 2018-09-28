73af35e4edc98e474c6c077b35786ba22ebf5ae0
Joseph Davies <joseph.davies@mail.utoronto.ca>
2018-09-28 04:10:07 -0400

    Changed the way messages are read and checked. Added debugging features.

--- content.js
+++ content.js
@@ -1,15 +1,45 @@
-var oldMessage;
+var oldMessageContents = [];
+var i;
+
+//Delete for actual implementation
+var itemsDetected = 0;
 
 function checkMessages() {
 
-    var latestMessage = document.getElementsByClassName("ingame-messages")[1].children[0];
+    var messageContents = [];
 
-    if ((oldMessage === null || oldMessage != latestMessage) && latestMessage.className == "bigbrother") {
+    for (i = 0; i < 2; i++) {
 
-        alert("Important message!!!");
+        messageContents[i] = document.getElementsByClassName("ingame-messages")[1].children[i];
+
     }
 
-    oldMessage = latestMessage;
+    //    var latestMessage = document.getElementsByClassName("ingame-messages")[1].children[0];
+    //    var msgContent = document.getElementsByClassName("body")[0];
+
+    if ((oldMessageContents.length === 0 || oldMessageContents[1] != messageContents[1]) && messageContents[0].className == "bigbrother") {
+
+        alert("Important message!!!");
+        itemsDetected++;
+
+    }
+
+    if (oldMessageContents.length > 0) {
+
+        var toDisplay = "old message content: " + oldMessageContents[1].innerHTML + "\<br\>new message content: " + messageContents[1].innerHTML + "\<br\> Are the two the same?: " + (oldMessageContents[1] == messageContents[1]) + "\<br\>Number of items detected so far: " + itemsDetected;
+
+        document.getElementsByClassName("ingame-right")[0].innerHTML = toDisplay;
+
+    }
+
+
+
+    for (i = 0; i < 2; i++) {
+
+        oldMessageContents[i] = messageContents[i];
+
+    }
+
 }
 
 setInterval(checkMessages, 500);
